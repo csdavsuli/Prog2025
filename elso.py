7514fe0ee02b5d8110a990d874275edd0ef63cf4
Csik Dávid <csik.davidka07@gmail.com>
2025-09-23 11:27:09 +0200

    09.23

--- elso.py
+++ elso.py
@@ -8,14 +8,26 @@ szamok=[]
 #szám generálás
 
 for i in range(100):
-    szam=random.randint(0,99)
+    szam=random.randint(0,100)
     szamok.append(szam)
 
 print(szamok)
 
-#egyszámjáték
-jatek=szam=0
-nemtalaltdb=0
-
 #kitalálandó szám beállítása
-kszam=random.randint(1,100)
+kszam=szamok[random.randint(0,len(szamok))]
+
+tipp=int(input("Kérek egy számot 1 és 100 között: "))
+if (tipp==kszam):
+        print("Gratulálok, eltaláltad a számot")
+elif (tipp<kszam):
+    print("A keresett szám nagyobb")
+else:
+    print("A keresett szám kisebb")
+while(tipp!=kszam):
+    tipp=int(input("Kérek egy számot 1 és 100 között: "))
+    if (tipp==kszam):
+        print("Gratulálok, eltaláltad a számot")
+    elif (tipp<kszam):
+        print("A keresett szám nagyobb")
+    else:
+        print("A keresett szám kisebb")
