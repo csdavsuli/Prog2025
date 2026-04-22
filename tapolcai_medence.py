f46bc9394e0096aa3e568a6816078311ea1398c7
Csik Dávid <csik.davidka07@gmail.com>
2026-04-22 07:50:13 +0200

    kommit

--- tapolcai_medence.py
+++ tapolcai_medence.py
@@ -34,11 +34,67 @@ def mondat_szavak_szama():
     
     print(f"A szövegben lévő mondatok száma: {mondatszam}")
     
+    mondat_lista = szoveg.split('\n')
+    
+    szavak_szama=0
+    for mondat in mondat_lista:
+        szavak_szama += len(mondat.split())
 
+    print(f"A szövegben lévő szavak száma: {szavak_szama}")
 
+def hegyek(szoveg):
+    hegyek_lista = []
 
+    szoveg = szoveg.replace('\n',' ')
+    szavak_lista = szoveg.split()
+    
+    for szo in szavak_lista:
+        if "HEGY" in szo.upper():
+            szo = szo.strip(',').strip('.').strip('!')
+            if szo not in hegyek_lista:
+                hegyek_lista.append(szo)
+    print(f"Hegyek:\n\t{hegyek_lista}")
+"""    
+def szocsere(szoveg):
 
+szoveg= szoveg.replace('Balaton', 'BALATON')
+szoveg = szoveg.replace('vulkán', 'tűzhányó')    
+"""
 
+def min_max_maganhangzo(szoveg):
+    maganhangzok = "aáeéiíoóöőuúüű"
+    maganhangzo_db = []
+    
+    
+    for i in range(len(maganhangzok)):
+        maganhangzo_db.append(szoveg.count(maganhangzok[i]))
+            
+    print(maganhangzo_db)
+    
+    mini = 0
+    maxi = 0
+    
+    for i in range(1, len(maganhangzo_db)):
+        if  maganhangzo_db[i] < maganhangzo_db[mini]:
+            mini=i
+        if maganhangzo_db[i] > maganhangzo_db[maxi]:
+            maxi=i
+    print(f"A legritkábban előforduló magánhangzó: {maganhangzok[mini]}")
+    print(f"A leggyakrabbban előforduló mássalhangzó: {maganhangzok[maxi]}")
+
+def tomorito(szoveg):
+    maganhangzok = "aáeéiíoóöőuúüű"
+    tomoritett = szoveg
+    for mghg in maganhangzok:
+        tomoritett = tomoritett.replace(mghg, '')
+
+    print(f'\nEredeti: {szoveg[0:80]}\n')
+    print(f"Tömörített:{tomoritett}")
 ########################
 
 cim_formazas()
+mondat_szavak_szama()
+hegyek(szoveg)
+#szocsere(szoveg)
+min_max_maganhangzo(szoveg)
+tomorito(szoveg)
